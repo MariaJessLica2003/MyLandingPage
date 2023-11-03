fdb94c26fd16807bd10ee51c574174bb26a4be3e
MariaJessLica2003 <mariajessamie.licaros@carsu.edu.ph>
2023-11-03 08:07:11 +0800

    Update the animation

---
+++ assets/js/important-script.js
@@ -0,0 +1,11 @@
+/**
+   * Hero type effect
+   */
+const typed = new Typed('.typed', {
+    strings: ["Hello", "World", "Typed.js"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+  });
+  
+
